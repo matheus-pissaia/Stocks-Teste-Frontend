344c37a70cbaa375d95fef8f1479276f7c524661
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-11-23 19:05:24 -0300

    Component Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--purple);
+`;
+
+export const Content = styled.div`
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 6rem 0;
+`;
