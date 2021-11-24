e78136a00fce70d1cdeaea523432eb73aa3cae26
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-11-24 00:20:42 -0300

    Component StockList

---
+++ src/components/StockList/index.tsx
@@ -0,0 +1,36 @@
+import { StockItem } from '../StockItem';
+
+import { Container } from './styles';
+
+const stocks = [
+  {
+    symbol: 'APPL',
+    price: 148.81,
+    graph: 'up'
+  },
+  {
+    symbol: 'NKE',
+    price: 375.99,
+    graph: 'down'
+  },
+  {
+    symbol: 'TSLA',
+    price: 1141.17,
+    graph: 'down'
+  },
+  {
+    symbol: 'AMZN',
+    price: 851.76,
+    graph: 'up'
+  }
+]
+
+export function StockList() {
+  return (
+    <Container>
+      {stocks.map(stock => {
+        return <StockItem key={stock.symbol} stockItem={stock} />
+      })}
+    </Container>
+  )
+}
---
+++ src/components/StockList/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.ul`
+  list-style: none;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
