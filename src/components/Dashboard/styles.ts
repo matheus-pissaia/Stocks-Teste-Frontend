ccb512ab2262de53e25a5d2f082361acd190ac8a
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-11-23 21:34:18 -0300

    Component Dashboard

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  max-width: 1120px;
+  margin: 0 auto;
+  margin-top: -4rem;
+
+  background: var(--gray-900);
+  border-radius: 1rem;
+
+  box-shadow: 0px 10px 60px rgba(113, 41, 204, 0.4);
+
+  ul {
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+`;
