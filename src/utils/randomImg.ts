5da562f27079680e1838679001aab97cda01bb95
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-11-25 23:32:31 -0300

    choose a random image

--- src/utils/randomImg.ts
+++ src/utils/randomImg.ts
@@ -1,3 +1,5 @@
+// Função que escolhe uma imagem aleatória a partir de um Array de imagens.
+
 export const randomImg = (images: Array<string>) => {
   return images[Math.floor(Math.random() * images.length)]
 }
